fdf58a7bc1170b0b3273c38aabc94156eb6a2e3a
esatyasar <esatyasar91@gmail.com>
2021-08-16 13:13:35 +0300

    style added

--- src/components/card/Card.js
+++ src/components/card/Card.js
@@ -1,13 +1,61 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 import { categories } from '../../helper/data'
 import './Card.css';
 
 
 function Card() {
     
+    const [overContent, setOverContent] = useState(true);
+
     return (
+
+        <div className="d-flex">
+            <div className="rounded-3 bg-warning m-auto col-4 ">
+                        <h3 className="lang bg-warning w-100 text-center my-3 fs-1">
+                            Languages
+                        </h3>
+                        <div className="col-sm-12 flex-wrap d-flex bg-warning my-2">
+
+                        <div className="box-container col-sm-12 d-flex justify-content-center align-item-center flex-wrap" onClick={() =>
+                        setOverContent(!overContent)}>
+                            {overContent ? (
+                                <>
+                                {categories.map((card) => {
+                                    return(
+                                        <div className="box col-sm-4 d-flex  align-items-center my-3 py-2 mx-3">
+                                            <div className="col-sm-12 d-flex flex-column align-items-center">
+                                                <img className="images mt-2" src={card.img} alt="javascript"/>
+                                                <p className="my-2 text-dark fw-bold fs-5">{card.name} </p>    
+                                            </div>  
+                                        </div>                         
+                                        )
+                                    }
+                                )}
+                                </>
+                            ):(
+                                <>
+                                {categories.map((card) => {
+                                    return(
+                                    <div className="box col-sm-4 d-flex  align-items-center my-3 py-2 mx-3">
+                                        <ul calssName="ul-box">
+                                            <li>{card.options[0]}</li>
+                                            <li>{card.options[1]}</li>
+                                            <li>{card.options[2]}</li>
+                                        </ul>
+                                    </div>
+                                    )
+                                })}
+                                </>
+                            )
+                        }
+                    </div>  
+                </div>             
+            </div>
+        </div>
+    )}        
         
-        <div className=" d-flex align-center">
+        
+        /* <div className=" d-flex align-center">
             <div className="rounded-3 bg-warning m-auto col-4">
                 <h3 className="lang bg-warning w-100 text-center my-3 fs-1">
                     Languages
@@ -53,8 +101,7 @@ function Card() {
                     </div>
                 </div>
             </div>
-        </div>
-    )
-}
+        </div> */
+  
 
 export default Card
