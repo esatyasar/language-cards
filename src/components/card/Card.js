e63c3e93e4795127cbc3ab5cbdd8bf6be6031549
esatyasar <esatyasar91@gmail.com>
2021-08-16 16:59:15 +0300

    create new component

--- src/components/card/Card.js
+++ src/components/card/Card.js
@@ -1,10 +1,10 @@
 import React, { useState } from 'react'
 import { categories } from '../../helper/data'
+import Item from "../Item"
 import './Card.css';
 
 
 function Card() {
-    const [overContent,setOverContent] = useState(true)
     return (
 
         <div className="d-flex">
@@ -14,39 +14,14 @@ function Card() {
                         </h3>
                         <div className="col-sm-12 flex-wrap d-flex bg-warning my-2">
 
-                        <div className="box-container col-sm-12 d-flex justify-content-center align-item-center flex-wrap" onClick={() =>
-                        setOverContent(!overContent)}>
-                            {overContent ? (
-                                <>
-                                {categories.map((card) => {
+                        <div className="box-container col-sm-12 d-flex justify-content-center align-item-center flex-wrap">
+                           
+                                {categories.map((card,index) => {
                                     return(
-                                        <div className="box col-sm-4 d-flex  align-items-center my-3 py-2 mx-3">
-                                            <div className="col-sm-12 d-flex flex-column align-items-center">
-                                                <img className="images mt-2" src={card.img} alt="javascript"/>
-                                                <p className="my-2 text-dark fw-bold fs-5">{card.name} </p>    
-                                            </div>  
-                                        </div>                         
+                                        <Item card = {card} key={index} />             
                                         )
                                     }
                                 )}
-                                </>
-                            ):(
-                                <>
-                               
-                                {categories.map((card) => {
-                                    return(
-                                    <div className="box col-sm-4 d-flex  align-items-center my-3 py-2 mx-3">
-                                        <ul calssName="ul-box">
-                                            <li>{card.options[0]}</li>
-                                            <li>{card.options[1]}</li>
-                                            <li>{card.options[2]}</li>
-                                        </ul>
-                                    </div>
-                                    )
-                                })}
-                                </>
-                            )
-                        }
                     </div>  
                 </div>             
             </div>
