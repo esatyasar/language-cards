8539bea6388e2ea0e8a1add3580fc79889f415e4
esatyasar <esatyasar91@gmail.com>
2021-08-17 12:50:23 +0300

    added responsive

--- src/components/Item.js
+++ src/components/Item.js
@@ -5,7 +5,7 @@ function Item({card}) {
     const [overContent,setOverContent] = useState(true)
     return (
     
-        <div className="box col-sm-4 d-flex  align-items-center my-3 py-2 mx-3"  onClick= {() =>
+        <div className="box col-sm-4 d-flex align-items-center justify-content-center my-3 py-2 mx-3"  onClick= {() =>
                  setOverContent(!overContent)
             }>
                 <div className="col-sm-12 d-flex flex-column align-items-center">{
